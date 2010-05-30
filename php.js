4005a87544a5c6ad82f40aca74259c62f58f6797
Nathan Hoad <nathan@nathanhoad.net>
2010-05-30 10:45:17 +1000

    Added 'extends' and 'class' to keywords for PHP

--- php.js
+++ php.js
@@ -9,7 +9,7 @@ CodeHighlighter.addStyle("php",{
 		exp  : /'[^']*'|"[^"]*"/
 	},
 	keywords : {
-		exp  : /\b(array|break|case|continue|default|die|do|echo|else|false|for|foreach|function|if|new|null|print|private|public|protected|return|static|switch|this|true|var|void|while|with)\b/
+		exp  : /\b(array|break|case|class|continue|default|die|do|echo|else|extends|false|for|foreach|function|if|new|null|print|private|public|protected|return|static|switch|this|true|var|void|while|with)\b/
 	},
 	variable : {
 	  exp : /(\$[A-Za-z0-9_!?]+)/
