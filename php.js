58555bbbc4ea1685d6607deee0c9db6752843e85
Nathan Hoad <nathan@bluedogtraining.com.au>
2011-05-09 10:09:11 +1000

    Added more keywords to the PHP highlighter

--- php.js
+++ php.js
@@ -9,7 +9,7 @@ CodeHighlighter.addStyle("php",{
 		exp  : /'[^']*'|"[^"]*"/
 	},
 	keywords : {
-		exp  : /\b(array|break|case|class|continue|default|die|do|echo|else|extends|false|for|foreach|function|if|new|null|print|private|public|protected|return|static|switch|this|true|var|void|while|with)\b/
+		exp  : /\b(abstract|array|break|case|class|continue|default|die|do|echo|else|extends|false|for|foreach|function|if|interface|new|null|print|private|public|protected|return|static|switch|this|true|var|void|while|with)\b/
 	},
 	variable : {
 	  exp : /(\$[A-Za-z0-9_!?]+)/
